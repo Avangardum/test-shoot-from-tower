ed0f38ea165c6b1eeb46f3b5a50d4899ddeff1f2
Silvian-73 <yurykuz65@gmail.com>
2021-02-24 14:54:30 +0300

    Implemented swithcing to the shooting mode

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -7,8 +7,11 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class PlayerBehaviour : MonoBehaviour
 {
+    private const bool ShouldDrawPath = false;
+    
     private NavMeshAgent _navMeshAgent;
-    private bool _shootingMode;
+    private bool _isInShootingMode;
+    private bool _isInShootingZone;
 
     [SerializeField] private LayerMask levelMask;
     
@@ -19,6 +22,16 @@ public class PlayerBehaviour : MonoBehaviour
 
     private void Update()
     {
+        Movement(); 
+        DrawPath();
+        
+    }
+
+    private void Movement()
+    {
+        if (_isInShootingMode)
+            return;
+        
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -27,6 +40,42 @@ public class PlayerBehaviour : MonoBehaviour
             {
                 _navMeshAgent.SetDestination(raycastHit.point);
             }
+        }
+
+        if (_navMeshAgent.remainingDistance == 0 && _isInShootingZone)
+            EnableShootingMode();
+    }
+
+    private void DrawPath()
+    {
+        if (!ShouldDrawPath)
+            return;
+        for (int i = 1; i < _navMeshAgent.path.corners.Length; i++)
+        {
+            Debug.DrawLine(_navMeshAgent.path.corners[i], _navMeshAgent.path.corners[i-1], Color.blue);
+        }
+    }
+
+    private void EnableShootingMode()
+    {
+        _isInShootingMode = true;
+        Debug.Log("(　-_･) ︻デ═一");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(1);
+        if (other.CompareTag("Shooting Zone"))
+        {
+            _isInShootingZone = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Shooting Zone"))
+        {
+            _isInShootingZone = false;
         }
     }
 }
