a8874f622ab08e8a5ab4f38df04b0e762523e424
Silvian-73 <yurykuz65@gmail.com>
2021-02-24 14:19:28 +0300

    Implemented player movement

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerBehaviour : MonoBehaviour
+{
+    private NavMeshAgent _navMeshAgent;
+    private bool _shootingMode;
+
+    [SerializeField] private LayerMask levelMask;
+    
+    private void Awake()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit raycastHit;
+            if (Physics.Raycast(ray, out raycastHit, Mathf.Infinity, levelMask))
+            {
+                _navMeshAgent.SetDestination(raycastHit.point);
+            }
+        }
+    }
+}
